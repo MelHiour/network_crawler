e567ef34222ee93c612823049c9216e024dd1f55
melhiour <melhiour@gmail.com>
2019-01-15 14:58:46 +0300

    Multiprocessing pinger added

--- connection_establisher/ios_connection_establisher.py
+++ connection_establisher/ios_connection_establisher.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
 
 import yaml
+import subprocess
 from pprint import pprint
+from concurrent.futures import ProcessPoolExecutor
 import netmiko 
+
+def ping_ip_address(ip):
+    pinger = subprocess.run(['ping', '-c', '3', '-n', ip], stdout=subprocess.DEVNULL)
+    if pinger.returncode == 0:
+        return {'alive':ip}
+    else:
+        return {'dead':ip}
+
+def ping_ip_threads(ips, limit=10):
+    with ProcessPoolExecutor(max_workers=limit) as executor:
+        pinger_result = list(executor.map(ping_ip_address, ips))
+    ip_list = {'alive':[], 'dead':[]}
+    for item in pinger_result:
+        if 'alive' in item.keys():
+            ip_list['alive'].append(item['alive'])
+        else:
+            ip_list['dead'].append(item['dead'])
+    return ip_list
 
 def ios_connection_establisher(host, creds_file): 
     with open(creds_file) as file:
@@ -20,10 +40,10 @@ def ios_connection_establisher(host, creds_file):
 def devices_from_file(device_file):
     with open(device_file) as file:
         result = file.read().split('\n')
-        return result
+        return result[0:-1]
 
 if __name__ == '__main__':
-    result = devices_from_file('devices')
-    #result = ios_connection_establisher('192.168.0.24', 'creds.yml')
-    print(result)
+    devices = devices_from_file('devices')
+    pinger = ping_ip_threads(devices)
+    print(pinger)
 				
