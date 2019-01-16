ea5defd8a892bf9cfa20f07d6b8b482019e2e611
Mikhail Ermakov <melhiour@mac.pc.hi>
2019-01-16 17:36:28 +0300

    argparser

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -3,11 +3,14 @@ from pprint import pprint
 import crawler_modules as cr
 
 parser = argparse.ArgumentParser(description="Crawler")
-parser.add_argument('--devices', action='store', dest='device_file', required=True, help='Path to device file')
-parser.add_argument('--creds', action='store', dest='creds_file', required=True, help='Path to file with credentials')
-parser.add_argument('--commands', action='store', dest='command_file', required=True, help='Path to file with comamnds list to be executed')
-parser.add_argument('--ping', action='store_true', dest='ping')
-parser.add_argument('--no-ping', action='store_false', dest='ping')
+parser.add_argument('--devices',
+                    action='store', dest='device_file', required=True, help='Path to device file')
+parser.add_argument('--creds',
+                    action='store', dest='creds_file', required=True, help='Path to file with credentials')
+parser.add_argument('--commands',
+                    action='store', dest='command_file', required=True, help='Path to file with comamnds list to be executed')
+parser.add_argument('--no-ping',
+                    action='store_false', dest='ping', help='Skip ping test')
 parser.set_defaults(ping = True)
 args = parser.parse_args()
 
