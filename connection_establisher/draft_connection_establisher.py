c1ea553a8ea4f8f2ee359c636de2351bb2687ed7
Mikhail Ermakov <melhiour@mac.pc.hi>
2019-01-16 10:20:40 +0300

    Refactoring

--- connection_establisher/draft_connection_establisher.py
+++ connection_establisher/draft_connection_establisher.py
@@ -14,15 +14,9 @@ def ping_ip_address(ip):
     else:
         return {'dead':ip}
 
-def ping_ip_threads(ips, type = 'process'):
-    if type == 'process':
-        with concurrent.futures.ProcessPoolExecutor(max_workers=len(ips)) as executor:
-            pinger_result = list(executor.map(ping_ip_address, ips))
-    elif type == 'thread':
-        with concurrent.futures.ThreadPoolExecutor(max_workers=len(ips)) as executor:
-            pinger_result = list(executor.map(ping_ip_address, ips))
-    else:
-        return ('Specify correct threading option. type = thread|process')
+def ping_ip_threads(ips):
+    with concurrent.futures.ProcessPoolExecutor(max_workers=len(ips)) as executor:
+        pinger_result = list(executor.map(ping_ip_address, ips))
     ip_list = {'alive':[], 'dead':[]}
     for item in pinger_result:
         if 'alive' in item.keys():
@@ -32,7 +26,11 @@ def ping_ip_threads(ips, type = 'process'):
     return ip_list
 
 def connection_maker(host, creds, command_file):
-    device_params = {'device_type': 'cisco_ios', 'ip': host, 'username': creds[0], 'password': creds[1],'secret': creds[1]}
+    device_params = {'device_type': 'cisco_ios',
+                    'ip': host,
+                    'username': creds[0],
+                    'password': creds[1],
+                    'secret': creds[1]}
     print('Tryin {}, {} on {}'.format(creds[0], creds[1], host))
     try:
         with netmiko.ConnectHandler(**device_params) as ssh:
@@ -52,12 +50,18 @@ def connection_maker_threads(host, creds_file, command_file):
         creds = yaml.load(file)
     creds_product = list(itertools.product(creds['usernames'], creds['passwords']))
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        grabber = list(executor.map(connection_maker, itertools.repeat(host), creds_product, itertools.repeat(command_file)))
+        grabber = list(executor.map(connection_maker,
+                                    itertools.repeat(host),
+                                    creds_product,
+                                    itertools.repeat(command_file)))
     return grabber
 
 def connection_maker_threads2(hosts, creds_file, command_file):
     with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-        grabber = list(executor.map(ios_connection_establisher, hosts, itertools.repeat(creds_file), itertools.repeat(command_file)))
+        grabber = list(executor.map(ios_connection_establisher,
+                                    hosts,
+                                    itertools.repeat(creds_file),
+                                    itertools.repeat(command_file)))
     return grabber
 
 def ios_connection_establisher(host, creds_file, command_file):
@@ -66,7 +70,11 @@ def ios_connection_establisher(host, creds_file, command_file):
     creds_product = list(itertools.product(creds['usernames'], creds['passwords']))
     output = {}
     for creds in creds_product:
-        device_params = {'device_type': 'cisco_ios', 'ip': host, 'username': creds[0], 'password': creds[1],'secret': creds[1]}
+        device_params = {'device_type': 'cisco_ios',
+                        'ip': host,
+                        'username': creds[0],
+                        'password': creds[1],
+                        'secret': creds[1]}
         try:
             with netmiko.ConnectHandler(**device_params) as ssh:
                 ssh.enable()
