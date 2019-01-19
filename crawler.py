8b34193d08f7f3178cf5c69a7d7813fd6e10b70a
Mikhail Ermakov <melhiour@mac.pc.hi>
2019-01-19 23:09:21 +0300

    Stat look has changed

--- crawler.py
+++ crawler.py
@@ -100,7 +100,8 @@ if result:
     status_list = [item[1] for item in brief_view]
     status_list_unique = set([item[1] for item in brief_view])
     count_list = [(status, status_list.count(status)) for status in status_list_unique]
-    print(tabulate(count_list, headers = ['STATUS', 'COUNT'], tablefmt='rst'))
+    print('INFO | Showing statistics information\n')
+    print(tabulate(count_list, headers = ['STATUS', 'COUNT']))
 
 end = datetime.datetime.now()
 
